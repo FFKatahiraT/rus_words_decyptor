af047170e9e759b44407777aada8654759ec42f4
Tim <58525817+FFKatahiraT@users.noreply.github.com>
2020-09-15 23:20:44 +0300

    Add files via upload

--- crypt21V2.py
+++ crypt21V2.py
@@ -14,14 +14,22 @@ def count_concidences(Func):
 
 def sort_keys():
 	return {k: v for k, v in sorted(count.items(), key=lambda item: item[1])};
-
+'''
+def find_character(word):
+	for j in range(len(word)): 
+    	if word[j] == 'о': 
+        	res = i + 1
+        	break
+'''
 change_letters = {}
 #FrequentlyUsedLetters = ['о', 'е', 'а', 'и','н','т','с','р','в','л','к','м','д','п','у','я','ы','ь','г','з','б','ч','й','х','ж','ш','ю', 'ц','щ','э','ф','ё']
 #FrequentlyUsedLetters = ['о','а','е','и','н','т','р','с','л','в','к','п','м','у','д','я','ы','ь','з','б','г','й','ч','ю','х','ж','ш','ц','щ','ф','э','ъ']
-FrequentlyUsedLetters = ['о','е','а','и','т','н','с','р','в','л','к','д','м','у','п','я','ы','ь','з','б','г','й','ч','ю','х','ж','ш','ц','щ','ф','э','ъ']
+FrequentlyUsedLetters = ['о','е','а','и','н','с','т','л','в','р','д','к','м','у','п','г','я','ь','б','з','ы','ч','й','ж','ш','х','ю','щ','ц','ф','э','ъ']
 
 ABC = ['а', 'б','в', 'г','д','е','ё', 'ж','з', 'и', 'й','к', 'л','м', 'н','о','п','р', 'с','т', 'у','ф', 'х','ц', 'ч','ш', 'щ','ъ', 'ы','ь', 'э','ю', 'я']
-delta, old_letters, new_letters, possible_shifts, letters, temp, available_variants = [], [], [], [], [], [],[]
+delta, old_letters, new_letters, possible_shifts, letters, temp, available_variants, words = [], [], [], [], [], [],[], []
+text_with_changed_letters = []
+
 
 for i in range(30):
 	try:
@@ -68,9 +76,26 @@ for k in range(len(temp)):
 		else:
 			pass
 	file_name = str('modified_crypt')+str(available_variants[k])+str('.txt')
+	text_with_changed_letters.append("".join(letters_list))
 	file = open(file_name,'w')
-	file.write("".join(letters_list))
+	file.write(text_with_changed_letters[k])
 	file.close()
+
+with open(str('word_rus.txt'), 'r') as data:
+	data_new = data.readlines()
+	for i in data_new:
+		words.append(i.replace('\n',''))
+
+# for k in range(len(temp)):
+# 	raw_words = text_with_changed_letters[k].split(' ')
+# 	for i in raw_words:
+# 		for m in words:
+# 			if len(m) == len(i) and :
+
+
+
+#print(words)
+
 exit()
 '''
 #The second way
