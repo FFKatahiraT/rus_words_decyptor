e8b9d2c27d65e6bf59f7affa6adf25ab320f101e
Tim <58525817+FFKatahiraT@users.noreply.github.com>
2020-09-16 20:36:59 +0300

    NEW FEATURES
    1) Added new function - encrypt
    2)  Added comments to the code
    3) The code became more readable

--- crypt21V2.py
+++ crypt21V2.py
@@ -1,3 +1,5 @@
+from random import shuffle
+
 def removekey(d, key):
     r = dict(d)
     del r[key]
@@ -12,62 +14,93 @@ def count_concidences(Func):
 	  		count[i] = 1
 	return count;
 
-def sort_keys():
-	return {k: v for k, v in sorted(count.items(), key=lambda item: item[1])};
-'''
-def find_character(word):
-	for j in range(len(word)): 
-    	if word[j] == 'о': 
-        	res = i + 1
-        	break
-'''
-change_letters = {}
-#FrequentlyUsedLetters = ['о', 'е', 'а', 'и','н','т','с','р','в','л','к','м','д','п','у','я','ы','ь','г','з','б','ч','й','х','ж','ш','ю', 'ц','щ','э','ф','ё']
-#FrequentlyUsedLetters = ['о','а','е','и','н','т','р','с','л','в','к','п','м','у','д','я','ы','ь','з','б','г','й','ч','ю','х','ж','ш','ц','щ','ф','э','ъ']
-FrequentlyUsedLetters = ['о','е','а','и','н','с','т','л','в','р','д','к','м','у','п','г','я','ь','б','з','ы','ч','й','ж','ш','х','ю','щ','ц','ф','э','ъ']
+def encrypt(key):
+	shuffle(key)
 
+def read_data(data_type):
+	letters = []
+	#READ DATA
+	for i in range(30):
+		try:
+			open_file = str(data_type)+str(i)+str('_crypt.txt')
+			with open(open_file, 'r') as data:
+				letters_in_text.append(data.read())
+				available_variants.append(i)
+		except:
+			pass
+
+	#SPLITTING A TEXT INTO LETTERS
+	for i in range(len(letters_in_text)):
+		letters += letters_in_text[i]
+	print(len(letters), ' quantity of letters')
+	count = count_concidences(letters)
+
+	#REMOVE NON_DICTIONARY CHARACTERS 
+	for key, value in count.items():
+		if key in ABC:
+			pass
+		else:
+			count = removekey(count, key)
+	
+	#SORTING LETTERS BY APPEARANCE FREQUENCY
+	keys_sorted = {k: v for k, v in sorted(count.items(), key=lambda item: item[1])};
+	print(keys_sorted, ' letters appearance frequency')
+	return keys_sorted, letters_in_text;
+
+
+def substitution_of_letters(letters_in_text, FrequentlyUsedLetters ,type_of_proccessing):
+	change_letters = {}
+	#CHANGING LETTERS WITH FREQUENTLY APPEARING ONES
+	i=len(FrequentlyUsedLetters)-1
+	for key, value in keys_sorted.items():
+		change_letters[key] = FrequentlyUsedLetters[i]
+		i-=1
+	for k in range(len(letters_in_text)):
+		letters_list = list(letters_in_text[k])
+		for i in range(len(letters_list)):
+			if letters_list[i] in ABC:
+				letters_list[i] = change_letters[letters_list[i]]
+			else:
+				pass
+
+		#DATA STORAGE
+		file_name = str(type_of_proccessing)+str(available_variants[k])+str('_crypt.txt')
+		text_with_changed_letters.append("".join(letters_list))
+		file = open(file_name,'w')
+		file.write(text_with_changed_letters[k])
+		file.close()
+
+#DICTIONARY AND DATA INITIALIZING
 ABC = ['а', 'б','в', 'г','д','е','ё', 'ж','з', 'и', 'й','к', 'л','м', 'н','о','п','р', 'с','т', 'у','ф', 'х','ц', 'ч','ш', 'щ','ъ', 'ы','ь', 'э','ю', 'я']
-delta, old_letters, new_letters, possible_shifts, letters, temp, available_variants, words = [], [], [], [], [], [],[], []
+delta, old_letters, new_letters, possible_shifts, letters_in_text, available_variants, words = [], [], [], [], [],[], []
 text_with_changed_letters = []
 
+type_of_proccessing = input('type_of_proccessing (encrypt/decrypt): ')
+while True:
+	if type_of_proccessing == 'decrypt':
+		#LIST OF FREQUENTLY APPEARING LETTERS
+		FrequentlyUsedLetters = ['о','е','а','и','н','с','т','л','в','р','д','к','м','у','п','г','я','ь','б','з','ы','ч','й','ж','ш','х','ю','щ','ц','ф','э','ъ']
 
-for i in range(30):
-	try:
-		open_file = str(i)+str('_crypt.txt')
-		with open(open_file, 'r') as data:
-			temp.append(data.read())
-			available_variants.append(i)
-	except:
-		pass
-for i in range(len(temp)):
-	letters += temp[i]
-print(len(letters), ' quantity of letters')
+		#READ DATA
+		keys_sorted, letters_in_text = read_data('')
 
-count = count_concidences(letters)
+		#LETTERS EXCHANGE
+		substitution_of_letters(letters_in_text, FrequentlyUsedLetters, type_of_proccessing)
+		print('files were decrypted')
+		break
 
-for key, value in count.items():
-	if key in ABC:
-		pass
+	elif type_of_proccessing == 'encrypt':
+		key = list(ABC)
+		encrypt(key)	#ENCRYPTING
+		keys_sorted, letters_in_text  = read_data('decrypt')
+		substitution_of_letters(letters_in_text, key, type_of_proccessing)
+		file = open('key.txt','w')
+		for i in range(len(ABC)):
+			file.write(str(ABC[i])+str(' = ')+str(key[i])+str('\n'))
+		file.close()
+		print('files were encrypted')
+		break
+
 	else:
-		count = removekey(count, key)
-
-keys_sorted = sort_keys()
-print(keys_sorted, ' letters appearance frequency')
-i=len(FrequentlyUsedLetters)-1
-for key, value in keys_sorted.items():
-	change_letters[key] = FrequentlyUsedLetters[i]
-	i-=1
-for k in range(len(temp)):
-	letters_list = list(temp[k])
-	for i in range(len(letters_list)):
-		if letters_list[i] in ABC:
-			old_letters.append(letters_list[i])
-			letters_list[i] = change_letters[letters_list[i]]
-			new_letters.append(letters_list[i])
-		else:
-			pass
-	file_name = str('modified_crypt')+str(available_variants[k])+str('.txt')
-	text_with_changed_letters.append("".join(letters_list))
-	file = open(file_name,'w')
-	file.write(text_with_changed_letters[k])
-	file.close()
+		print('write it again, there is a mistake!')
+		type_of_proccessing = input('type_of_proccessing (encrypt/decrypt): ')
